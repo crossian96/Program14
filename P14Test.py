3b08200ff8a8291535f0db9b52bc5ece168cda66
Sliverr <doh.parks@gmail.com>
2016-04-26 10:01:03 -0600

    Cleaned up code

--- P14Test.py
+++ P14Test.py
@@ -1,10 +1,11 @@
 from modCustomSet import *
 
 
-a1=CustomSet([1,1,2,3,4,4,5])
-print(str(a1))
-z=(CustomSet.brackets(a1))
-print(z)
-CustomSet.contains(a1, 4)
+a1=CustomSet([1,1,2,3,4,4,5]) #Method 1: Constructing Object
+print(a1)
+
+print(CustomSet.brackets(a1)) #Method 9
+
+CustomSet.contains(a1, 4)     #Method 5
 CustomSet.contains(a1, 6)
 
